358573f7d074f955615e1a7cb16e30858d7aa412
Andrea Francia <andrea@andreafrancia.it>
2017-11-25 19:59:10 +0100

    Save.

--- lib/dingy.rb
+++ lib/dingy.rb
@@ -1,3 +1,4 @@
+require 'time'
 def format_time time
   return 'Rang!' if time < 0
   '%02d:%02d' % [time/60, time%60]
--- spec/dingy_spec.rb
+++ spec/dingy_spec.rb
@@ -15,7 +15,6 @@ describe 'how to calculate remaining time' do
     expect(remainig_time '9:00', '9:00').to eq(0*60)
     expect(remainig_time '9:01', '9:00').to eq(1*60)
   end
-  require 'time'
 end
 describe 'how to format the remaining time'  do
   it do
@@ -31,14 +30,33 @@ end
 #                     bin/dingy  -ok-> stdout 25:00
 # ring_time 9:25 -->
 describe 'walking skeleton' do
+  before do
+    require 'fileutils'
+    FileUtils.rm_f 'next-alarm.txt'
+  end
   it do
-    env = {}
+    env['NOW'] = '9:00'
+
+    out = run_command 'bin/dingy --start'
+
+    expect(out).to eq('')
+    expect(File.exists?('next-alarm.txt')).
+      to be_truthy
+    expect(Time.parse(File.read('next-alarm.txt'))).
+      to eq(Time.parse('9:25'))
+  end
+  it do
     env['NOW'] = '9:00'
     File.write('next-alarm.txt', '9:25')
 
-    out, _ = Open3.capture2e env, 'bin/dingy'
+    out = run_command 'bin/dingy'
 
-    expect(out.chomp).to eq('9:25')
+    expect(out).to eq('25:00')
   end
+  def run_command command
+    out, _ = Open3.capture2e env, command
+    out.chomp
+  end
+  let(:env) { {} }
   require 'open3'
 end
